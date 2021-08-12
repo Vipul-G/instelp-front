63c3d46d69cfb6c8fb5f3c2ba89dc6a4832eb570
Vipul-Antino <vipul@antino.io>
2021-08-12 22:50:23 +0530

    _

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditorComponent } from './components/editor/editor.component';
 
 const routes: Routes = [
-  { path: '', component: EditorComponent }
+  { path: '', component: EditorComponent } 
 ];
 
 @NgModule({
